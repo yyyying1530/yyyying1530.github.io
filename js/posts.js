0bcdde2043cf4496c6fd90ba12aa2efde0e923d2
yyyying1530 <hanyuchen_007@163.com>
2021-08-04 23:10:32 +0800

    Site updated: 2021-08-04 23:10:32

--- js/posts.js
+++ js/posts.js
@@ -1,1 +1,1 @@
-[{"title":"Hello World","date":"2021.08.04","uri":"2021/08/04/hello-world/","excerpt":""}]
+[{"title":"AAA","date":"2021.08.04","uri":"2021/08/04/AAA-1/","excerpt":""},{"title":"Hello World","date":"2021.08.04","uri":"2021/08/04/hello-world/","excerpt":""}]
